0b90cd2bdc127281dec9a29f516cca565cbecfa2
alejandr2004 <446916.joan23@fje.edu>
2025-04-07 19:56:33 +0200

    Panel de administrador funcional + vista CRUD usuarios

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::redirect('/', '/home');
 
@@ -13,3 +14,8 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/login' , [AuthController::class, 'login'])->name('login');
 
 
+Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users');
+
+Route::get('/admin', function () {
+    return view('admin.index');
+})->name('admin.index');
