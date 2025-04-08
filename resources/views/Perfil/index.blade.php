424205b8de3fd16bdb39ea378525827be26e006d
RobertoNobleMaestro <betonm2005@gmail.com>
2025-04-08 15:46:48 +0200

    enlaces

--- resources/views/Perfil/index.blade.php
+++ resources/views/Perfil/index.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    
+    <link rel="stylesheet" href="{{ asset('css/perfil/style.css') }}">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <title>Perfil</title>
@@ -127,6 +128,7 @@
 </div>
 </div>
 </body>
+<script src="{{ asset('js/perfil.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
