e5ba7ac4f5298d3c9f762bea1201d0300708dfc0
RobertoNobleMaestro <betonm2005@gmail.com>
2025-04-23 17:10:35 +0200

    bd errores

--- database/seeders/TipoSeeder.php
+++ database/seeders/TipoSeeder.php
@@ -9,11 +9,11 @@ class TipoSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('tipos')->insert([
-            ['nombre_tipo' => 'Coche', 'descripcion' => 'Vehículo de cuatro ruedas para transporte de personas'],
-            ['nombre_tipo' => 'Moto', 'descripcion' => 'Vehículo de dos ruedas'],
-            ['nombre_tipo' => 'Furgoneta', 'descripcion' => 'Vehículo para transporte de mercancías ligeras'],
-            ['nombre_tipo' => 'Camión', 'descripcion' => 'Vehículo de gran tamaño para transporte de mercancías pesadas'],
+        DB::table('tipo')->insert([
+            ['nombre' => 'Coche'],
+            ['nombre' => 'Moto'],
+            ['nombre' => 'Furgoneta'],
+            ['nombre' => 'Camión'],
         ]);
     }
 }
