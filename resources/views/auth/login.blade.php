7d548e5ac56f5d0d9c4864d71815ca1531f676ec
RobertoNobleMaestro <betonm2005@gmail.com>
2025-05-19 15:36:35 +0200

    estilos bugs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,21 +10,20 @@
         <h2 class="login"> Iniciar Sesión </h2>
         <form action="{{route('login.post')}}" method="POST" id="loginForm">
             @csrf
-            <label for="email">Email</label><br>
+            <label for="email">Email</label>
             <input type="text" name="email" id="email" placeholder="usuario@gmail.com">
             <span class="error_message" id="error_email"></span>
-            <br>
-            <label for="password">Contraseña</label><br>
+            <label for="password">Contraseña</label>
             <input type="password" name="password" id="password" placeholder="********">
             <span class="error_message" id="error_password"></span>
             <br>
             <br>
             <button type="submit" id="login" disabled>Iniciar Sesión</button><br>
             <a href="{{route('register')}}" class=""> Registrarse </a>
-            <a href="{{ route('login.google') }}" class="google-login-button">
+            <!-- <a href="{{ route('login.google') }}" class="google-login-button">
                 <img src="{{ asset('img/google-icon.svg') }}" alt="Google Icon" style="width:20px; margin-right:10px;">
                 Iniciar sesión con Google
-            </a>
+            </a> -->
             <a href="{{ route('password.request') }}" class="forgot-password-link" style="display: block; margin-top: 15px; color: #9F17BD; text-decoration: none;">
                 <i class="fas fa-key" style="margin-right: 5px;"></i> ¿Has olvidado la contraseña?
             </a>
